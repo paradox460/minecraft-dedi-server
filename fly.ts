3b2a90d7649e0d25e4b023d03fbcca05910b6f1b
Jeff Sandberg <paradox460@gmail.com>
2024-07-10 15:35:07 -0600

    Fully TypeScriptize the fly.toml config

--- fly.ts
+++ fly.ts
@@ -1,127 +1,136 @@
 #! /usr/bin/env -S deno run --allow-write
 
-export {};
+import TOML from "npm:smol-toml@1.2";
 
-// Fly supports haProxy style ip forwarding, but it needs a few config changes
-// to make work
-// Specifically, you need to make it so Paper, the minecraft server, is aware of
-// the proxy running, so it can remap connections.
-// This handles all that automatically
-const enableProxy = true;
+export { };
 
-const preamble = `
-app = "pdx-minecraft"
-primary_region = "phx"
-
-[build]
-# image = "itzg/minecraft-server:latest"
-dockerfile = "Dockerfile"
-
-[mounts]
-source = "pdx_minecraft_data"
-destination = "/data"
-initial_size = "15"
-
-
-[[vm]]
-size="shared-cpu-8x"
-memory="8gb"
-cpus=4
-
-[[services]]
-internal_port = 25565
-protocol ="tcp"
-auto_stop_machines=false
-auto_start_machines=false
-[[services.ports]]
-port = 25565
-${flyProxyConfig()}
-
-[env]
-# General
-MAX_MEMORY="7G"
-TZ="America/Denver"
-ENABLE_ROLLING_LOGS="TRUE"
-CREATE_CONSOLE_IN_PIPE="true"
-REPLACE_ENV_VARIABLE_PREFIX=""
-
-# Minecraft Base
-EULA="TRUE"
-TYPE="paper"
-VERSION="1.20.6"
-SERVER_NAME="PDX"
-MOTD="wew lad"
-DIFFICULTY="normal"
-ICON="/data/icon.png"
-OVERWRITE_ICON="TRUE"
-ENFORCE_SECURE_PROFILE="FALSE"
-FORCE_GAMEMODE="FALSE"
-INITIAL_ENABLED_PACKS="update_1_21,bundle"
+const
+  // Fly supports haProxy style ip forwarding, but it needs a few config changes
+  // to make work
+  // Specifically, you need to make it so Paper, the minecraft server, is aware of
+  // the proxy running, so it can remap connections.
+  // This handles all that automatically
+  enableProxy = true,
+  plugins = [
+    "https://cdn.modrinth.com/data/1u6JkXh5/versions/VaMk0SQH/worldedit-bukkit-7.3.2.jar",
+    "https://cdn.modrinth.com/data/Lu3KuzdV/versions/llmrc4cl/CoreProtect-22.4.jar",
+    "https://cdn.modrinth.com/data/MubyTbnA/versions/vbGiEu4k/FreedomChat-Paper-1.6.0.jar",
+    "https://cdn.modrinth.com/data/RPOSBQgq/versions/E7jCNr0p/itemswapperplugin-0.2.1-SNAPSHOT.jar",
+    "https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/build/libs/ProtocolLib.jar",
+    "https://github.com/jmattingley23/AppleSkinSpigot/releases/download/latest/AppleSkinSpigot-1.2.0.jar",
+    "https://github.com/MiniPlaceholders/MiniPlaceholders/releases/download/2.2.4/MiniPlaceholders-Paper-2.2.4.jar",
+    "https://hangarcdn.papermc.io/plugins/EngineHub/CraftBook/versions/5.0.0-beta-03/PAPER/craftbook-bukkit-5.0.0-beta-03.jar",
+    "https://hangarcdn.papermc.io/plugins/LOOHP/ImageFrame/versions/1.7.9/PAPER/ImageFrame-1.7.9.0.jar",
+    "https://hangarcdn.papermc.io/plugins/Multiverse/Multiverse-Core/versions/4.3.12/PAPER/multiverse-core-4.3.12.jar",
+    "https://hangarcdn.papermc.io/plugins/Multiverse/Multiverse-Inventories/versions/4.2.6/PAPER/multiverse-inventories-4.2.6.jar",
+    "https://hangarcdn.papermc.io/plugins/Multiverse/Multiverse-NetherPortals/versions/4.2.3/PAPER/multiverse-netherportals-4.2.3.jar",
+    "https://hangarcdn.papermc.io/plugins/Multiverse/Multiverse-Portals/versions/4.2.3/PAPER/multiverse-portals-4.2.3.jar",
+    "https://hangarcdn.papermc.io/plugins/NanoFlux/Networks/versions/2.1.8/PAPER/Networks-2.1.8.jar",
+    "https://hangarcdn.papermc.io/plugins/Vicarious/Carbon/versions/3.0.0-beta.27/PAPER/carbonchat-paper-3.0.0-beta.27.jar",
+  ],
+  spigetPlugins = [
+    "https://www.spigotmc.org/resources/chestsort-api.59773/",
+    "https://www.spigotmc.org/resources/invunload.60095/",
+    "https://www.spigotmc.org/resources/luckperms.28140/",
+    "https://www.spigotmc.org/resources/placeholderapi.6245/",
+    "https://www.spigotmc.org/resources/slot-machine.22023/",
+    "https://www.spigotmc.org/resources/cmilib.87610/",
+  ],
 
 
-# Auto Stop
-ENABLE_AUTOSTOP="TRUE"
-AUTOSTOP_TIMEOUT_EST="600"
-AUTOSTOP_TIMEOUT_INIT="600"
-AUTOSTOP_PKILL_USE_SUDO="TRUE"
-USES_PROXY_PROTOCOL="${enableProxy}"
 
-# Gameplay
-ALLOW_FLIGHT="TRUE"
-ENABLE_COMMAND_BLOCK="TRUE"
-SNOOPER_ENABLED="FALSE"
-SEED="5539391629669013873"
+const generalEnv = {
+  MAX_MEMORY: "7G",
+  TZ: "America/Denver",
+  ENABLE_ROLLING_LOGS: "TRUE",
+  CREATE_CONSOLE_IN_PIPE: "true",
+  REPLACE_ENV_VARIABLE_PREFIX: "",
+}
 
-PATCH_DEFINITIONS="/config_patches/"
-`;
+const minecraftEnv = {
+  EULA: "TRUE",
+  TYPE: "paper",
+  VERSION: "1.20.6",
+  SERVER_NAME: "PDX",
+  MOTD: "wew lad",
+  DIFFICULTY: "normal",
+  ICON: "/data/icon.png",
+  OVERWRITE_ICON: "TRUE",
+  ENFORCE_SECURE_PROFILE: "FALSE",
+  FORCE_GAMEMODE: "FALSE",
+  INITIAL_ENABLED_PACKS: "update_1_21,bundle",
+  ALLOW_FLIGHT: "TRUE",
+  ENABLE_COMMAND_BLOCK: "TRUE",
+  SNOOPER_ENABLED: "FALSE",
+  SEED: "5539391629669013873",
+  PATCH_DEFINITIONS: "/config_patches/",
+}
 
-const plugins = [
-  "https://cdn.modrinth.com/data/1u6JkXh5/versions/VaMk0SQH/worldedit-bukkit-7.3.2.jar",
-  "https://cdn.modrinth.com/data/Lu3KuzdV/versions/llmrc4cl/CoreProtect-22.4.jar",
-  "https://cdn.modrinth.com/data/MubyTbnA/versions/vbGiEu4k/FreedomChat-Paper-1.6.0.jar",
-  "https://cdn.modrinth.com/data/RPOSBQgq/versions/E7jCNr0p/itemswapperplugin-0.2.1-SNAPSHOT.jar",
-  "https://ci.dmulloy2.net/job/ProtocolLib/lastSuccessfulBuild/artifact/build/libs/ProtocolLib.jar",
-  "https://download.luckperms.net/1549/bukkit/loader/LuckPerms-Bukkit-5.4.134.jar",
-  "https://github.com/jmattingley23/AppleSkinSpigot/releases/download/latest/AppleSkinSpigot-1.2.0.jar",
-  "https://github.com/MiniPlaceholders/MiniPlaceholders/releases/download/2.2.4/MiniPlaceholders-Paper-2.2.4.jar",
-  "https://hangarcdn.papermc.io/plugins/EngineHub/CraftBook/versions/5.0.0-beta-03/PAPER/craftbook-bukkit-5.0.0-beta-03.jar",
-  "https://hangarcdn.papermc.io/plugins/LOOHP/ImageFrame/versions/1.7.9/PAPER/ImageFrame-1.7.9.0.jar",
-  "https://hangarcdn.papermc.io/plugins/Multiverse/Multiverse-Core/versions/4.3.12/PAPER/multiverse-core-4.3.12.jar",
-  "https://hangarcdn.papermc.io/plugins/Multiverse/Multiverse-Inventories/versions/4.2.6/PAPER/multiverse-inventories-4.2.6.jar",
-  "https://hangarcdn.papermc.io/plugins/Multiverse/Multiverse-NetherPortals/versions/4.2.3/PAPER/multiverse-netherportals-4.2.3.jar",
-  "https://hangarcdn.papermc.io/plugins/Multiverse/Multiverse-Portals/versions/4.2.3/PAPER/multiverse-portals-4.2.3.jar",
-  "https://hangarcdn.papermc.io/plugins/NanoFlux/Networks/versions/2.1.8/PAPER/Networks-2.1.8.jar",
-  "https://hangarcdn.papermc.io/plugins/Vicarious/Carbon/versions/3.0.0-beta.27/PAPER/carbonchat-paper-3.0.0-beta.27.jar",
-].join(",");
+const autoStopEnv = {
+  ENABLE_AUTOSTOP: "TRUE",
+  AUTOSTOP_TIMEOUT_EST: "600",
+  AUTOSTOP_TIMEOUT_INIT: "600",
+  AUTOSTOP_PKILL_USE_SUDO: "TRUE",
+  USES_PROXY_PROTOCOL: enableProxy ? "TRUE" : "FALSE",
+}
 
-const spiget_plugins = [
-  "https://www.spigotmc.org/resources/chestsort-api.59773/",
-  "https://www.spigotmc.org/resources/invunload.60095/",
-  "https://www.spigotmc.org/resources/luckperms.28140/",
-  "https://www.spigotmc.org/resources/placeholderapi.6245/",
-  "https://www.spigotmc.org/resources/slot-machine.22023/",
-].map((x) => x.match(/.*?\.(\d+)\/$/)![1]!).join(",");
+const flyProxyConf = enableProxy ? {
+  handlers: ["proxy_proto"],
+  proxy_proto_options: { version: "v2" }
+} : {};
+
+
+const config = {
+  app: "pdx-minecraft",
+  primary_region: "phx",
+  build: { dockerfile: "Dockerfile" },
+  mounts: {
+    source: "pdx_minecraft_data",
+    destination: "/data",
+    initial_size: "15"
+  },
+  vm: [{ size: "shared-cpu-8x", memory: "8gb", cpus: 4 }],
+  services: [
+    {
+      internal_port: 25565,
+      protocol: "tcp",
+      auto_stop_machines: false,
+      auto_start_machines: false,
+      ports: [
+        {
+          port: 25565,
+          ...flyProxyConf,
+        }
+      ]
+    },
+  ],
+  env: {
+    ...generalEnv,
+    ...minecraftEnv,
+    ...autoStopEnv,
+    ...pluginEnvs(),
+
+  }
+}
+
+function pluginEnvs() {
+  const pluginSum = [
+    ...plugins,
+  ]
+  return {
+    PLUGINS: pluginSum.join(","),
+    SPIGET_RESOURCES: spigetPlugins.map((x) => x.match(/.*?\.(\d+)\/$/)![1]!).join(",")
+  }
+}
 
 const manual_comment = `
 # THIS FILE IS AUTOMATICALLY GENERATED VIA THE fly.ts SCRIPT
 # YOU SHOULD NOT EDIT IT BY HAND
 `;
 
-function flyProxyConfig(): string {
-  if (enableProxy) {
-    return `
-handlers = ["proxy_proto"]
-proxy_proto_options = { version = "v2"}
-    `.trim();
-  } else return "";
-}
-
-let outputs = [
+const outputs = [
   manual_comment,
-  preamble,
-  "",
-  `PLUGINS="${plugins}"`,
-  `SPIGET_RESOURCES="${spiget_plugins}"`,
+  TOML.stringify(config),
 ].map((x) => x.trim()).join("\n");
 
 await Deno.writeTextFile("fly.toml", outputs);
